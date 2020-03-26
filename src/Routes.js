90e9c4b7804ce1705c04c2b5f2099f1303708699
lucazweb <lucazwebmail@gmail.com>
2020-03-25 23:43:56 -0300

    Adds Register page and route

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 const Routes = () => (  
   <Router>
     <Switch>
-      <Route patch='/' component={Login} />
+      <Route path='/' exact component={Login} />
+      <Route path='/register' component={Register} />
     </Switch>
   </Router>
 );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
-
+import { Link } from 'react-router-dom';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 import './styles.css';
@@ -13,10 +13,10 @@ const Login = () => (
         <h1>Faça seu Login</h1>
           <input type="text" placeholder="ONG ID"/>
           <button className="button" type="submit">Entrar</button>
-        <a href="/register">
+        <Link to="/register">
           <FiLogIn size={16} color="#E02041" />
           Não tenho cadastro
-        </a>
+        </Link>
       </form>
     </section>
     <img src={heroesImg} alt="Heroes"/>
