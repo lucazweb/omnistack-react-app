258c360ccb03c2ce40dc04021114acfa3013ea5b
lucazweb <lucazwebmail@gmail.com>
2020-03-26 00:17:46 -0300

    Add NewIncidents page and route

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident'
 
 const Routes = () => (  
   <Router>
@@ -10,6 +11,7 @@ const Routes = () => (
       <Route path='/' exact component={Login} />
       <Route path='/register' component={Register} />
       <Route path='/profile' component={Profile} />
+      <Route path='/incidents/new' component={NewIncident} />
     </Switch>
   </Router>
 );
