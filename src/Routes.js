a781f3c49c43342b9de52f77e1ddc854e9452f8b
lucazweb <lucazwebmail@gmail.com>
2020-03-26 00:06:30 -0300

    Adds Profile page and route

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 const Routes = () => (  
   <Router>
     <Switch>
       <Route path='/' exact component={Login} />
       <Route path='/register' component={Register} />
+      <Route path='/profile' component={Profile} />
     </Switch>
   </Router>
 );
